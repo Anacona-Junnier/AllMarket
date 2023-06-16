44712c5519f5d7d7f1880e7f3bf4763a6601655e
janacona <janacona@unicauca.edu.co>
2023-06-15 23:22:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/allmarket/pantallaInicioSesion.java
@@ -0,0 +1,14 @@
+package com.example.allmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallaInicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio_sesion);
+    }
+}
